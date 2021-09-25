848871713e89584c61ed9a3310db15d15f46d734
LiuDaniu1997 <woaireus@gamil.com>
2021-09-25 09:37:44 +0200

    # update article list with new card style

--- frontend/src/components/ArticleList.vue
+++ frontend/src/components/ArticleList.vue
@@ -1,33 +1,52 @@
 <template>
-  <div v-for="article in info.results" v-bind:key="article.url" id="articles">
-    <div class="grid" :style="gridStyle(article)">
-      <div
-          class="image-container"
-      >
-        <img :src="imageIfExists(article)" alt="" class="image">
-      </div>
+  <div class="container">
+      <div v-for="article in info.results" v-bind:key="article.url" id="articles" class="card my-3 ">
+        <div class="row g-0">
+          <div class="col-md-4">
+            <img :src="imageIfExists(article)" class="img-fluid rounded-start" alt="image">
+          </div>
+          <div class="col-md-8">
+            <div class="card-body">
+              <!-- start title-->
+              <h5 class="card-title">
+                {{ article.title }}
+              </h5>
+              <!-- end title-->
 
-      <div>
-        <div>
-                    <span
-                        v-if="article.category !== null"
-                        class="category"
-                    >
-                        {{ article.category.title }}
-                    </span>
-          <span v-for="tag in article.tags" v-bind:key="tag" class="tag">{{ tag }}</span>
+              <!-- start category-->
+
+              <span
+                  v-if="article.category !== null"
+                  class="badge rounded-pill bg-primary"
+              >
+                  {{ article.category.title }}
+                  </span>
+              <!-- end category-->
+
+              <!-- start tags-->
+              <span v-for="tag in article.tags" v-bind:key="tag" class="badge rounded-pill bg-secondary">
+                {{ tag }}
+              </span>
+              <!-- end tags-->
+
+              <div class="d-flex align-items-center justify-content-between" id="card_bottom">
+                <!-- start created time-->
+                <div>
+                  <div>This Article is posted at: {{ formatted_time(article.created) }}</div>
+                </div>
+                <!-- end created time-->
+                <router-link
+                  :to="{ name: 'ArticleDetail', params: { id: article.id }}"
+                  class="btn btn-light"
+                >
+                  Learn More
+                </router-link>
+              </div>
+
+            </div>
+          </div>
         </div>
-        <div class="a-title-container">
-          <router-link
-              :to="{ name: 'ArticleDetail', params: { id: article.id }}"
-              class="article-title"
-          >
-            {{ article.title }}
-          </router-link>
-        </div>
-        <div>{{ formatted_time(article.created) }}</div>
       </div>
-    </div>
   </div>
 
 
@@ -92,173 +111,15 @@ export default {
       formatted_time,
     }
   },
-
-  //
-  //
-  // 选项式 API 章节内容
-  //
-  //
-
-  // data: function () {
-  //     return {
-  //         info: ''
-  //     }
-  // },
-  // mounted() {
-  //     this.get_article_data()
-  // },
-  // methods: {
-  // imageIfExists(article) {
-  //     if (article.avatar) {
-  //         return article.avatar.content
-  //     }
-  // },
-  // gridStyle(article) {
-  //     if (article.avatar) {
-  //         return {
-  //             display: 'grid',
-  //             gridTemplateColumns: '1fr 4fr'
-  //         }
-  //     }
-  // },
-  // formatted_time: function (iso_date_string) {
-  //     const date = new Date(iso_date_string);
-  //     return date.toLocaleDateString()
-  // },
-  // is_page_exists(direction) {
-  //     if (direction === 'next') {
-  //         return this.info.next !== null
-  //     }
-  //     return this.info.previous !== null
-  // },
-  // get_page_param: function (direction) {
-  //     try {
-  //         let url_string;
-  //         switch (direction) {
-  //             case 'next':
-  //                 url_string = this.info.next;
-  //                 break;
-  //             case 'previous':
-  //                 url_string = this.info.previous;
-  //                 break;
-  //             default:
-  //                 return this.$route.query.page
-  //         }
-  //
-  //         const url = new URL(url_string);
-  //         return url.searchParams.get('page')
-  //     }
-  //     catch (err) {
-  //         return
-  //     }
-  // },
-  // get_path: function (direction) {
-  //     let url = '';
-  //
-  //     try {
-  //         switch (direction) {
-  //             case 'next':
-  //                 if (this.info.next !== undefined) {
-  //                     url += (new URL(this.info.next)).search
-  //                 }
-  //                 break;
-  //             case 'previous':
-  //                 if (this.info.previous !== undefined) {
-  //                     url += (new URL(this.info.previous)).search
-  //                 }
-  //                 break;
-  //         }
-  //     }
-  //     catch {
-  //         return url
-  //     }
-  //
-  //     return url
-  // },
-  // get_article_data: function () {
-  //     let url = '/api/article';
-  //
-  //     let params = new URLSearchParams();
-  //     params.appendIfExists('page', this.$route.query.page);
-  //     params.appendIfExists('search', this.$route.query.search);
-  //
-  //     const paramsString = params.toString();
-  //     if (paramsString.charAt(0) !== '') {
-  //         url += '/?' + paramsString
-  //     }
-  //
-  //     axios
-  //         .get(url)
-  //         .then(response => (this.info = response.data))
-  // }
-  // },
-  // watch: {
-  //     $route() {
-  //         this.get_article_data()
-  //     }
-  // }
 }
 
 </script>
 
 <style scoped>
-.image {
-  width: 180px;
-  border-radius: 10px;
-  box-shadow: darkslategrey 0 0 12px;
-}
-
-.image-container {
-  width: 200px;
-}
-
-.grid {
-  padding-bottom: 10px;
-}
-
-#articles {
-  padding: 10px;
-}
-
-.article-title {
-  font-size: large;
-  font-weight: bolder;
-  color: black;
-  text-decoration: none;
-}
-
-.a-title-container {
-  padding: 5px 0 5px 0;
-}
-
-.category {
-  padding: 5px 10px 5px 10px;
-  margin: 5px 5px 5px 0;
-  font-family: Georgia, Arial, sans-serif;
-  font-size: small;
-  background-color: darkred;
-  color: whitesmoke;
-  border-radius: 15px;
-
-}
-
-.tag {
-  padding: 2px 5px 2px 5px;
-  margin: 5px 5px 5px 0;
-  font-family: Georgia, Arial, sans-serif;
-  font-size: small;
-  background-color: #4e4e4e;
-  color: whitesmoke;
-  border-radius: 5px;
-}
 
 #paginator {
   text-align: center;
   padding-top: 50px;
-}
-
-a {
-  color: black;
 }
 
 .current-page {
@@ -268,4 +129,25 @@ a {
   padding-right: 10px;
 }
 
+/*define the size of img in card*/
+.img-fluid {
+  width: 100%;
+  height: 15vw;
+  object-fit: cover;
+}
+
+/*start hover effect*/
+.card{
+  border-radius: 4px;
+  background: #fff;
+  box-shadow: 0 6px 10px rgba(0,0,0,.08), 0 0 6px rgba(0,0,0,.05);
+  transition: .3s transform cubic-bezier(.155,1.105,.295,1.12),.3s box-shadow,.3s -webkit-transform cubic-bezier(.155,1.105,.295,1.12);
+  cursor: pointer;
+}
+
+.card:hover{
+  transform: scale(1.05);
+  box-shadow: 0 10px 20px rgba(0,0,0,.12), 0 4px 8px rgba(0,0,0,.06);
+}
+/*end hover effect*/
 </style>
